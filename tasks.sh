7ff7fe1c30542d7527010439887eb6ca8f6812c9
Raul Varandela <raulvarandela@protonmail.com>
2022-08-25 18:17:26 +0200

    Crontab finishing

--- tasks.sh
+++ tasks.sh
@@ -5,27 +5,27 @@
 USER = 'raul'
 
 #mastodon
-*/15 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon publishText #no tira
-*/60 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon publishPhoto #idem
-*/30 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon boost # no tira por el propio metodo
-30 15 * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon replyComments #no tira
+*/15 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon publishText
+*/60 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon publishPhoto
+*/30 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon boost
+30 15 * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon replyComments
 */60 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon publishComment
 */10 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon like
 0 15 * * 1 /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon followUsers
-30 20 * * 1 /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon followBack #no funciona en metodo en sí
-0 15 1 * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon createAccount #no tira
+30 20 * * 1 /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon followBack
+0 15 1 * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py mastodon createAccount
 
 #twitter
-*/15 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter publishText #no tira
-*/60 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter publishPhoto #no tira
+*/15 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter publishText
+*/60 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter publishPhoto
 */30 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter boost
-30 15 * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter replyComments #no tira
-*/60 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter publishComment #no tira
+30 15 * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter replyComments
+*/60 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter publishComment
 */10 * * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter like
 0 15 * * 1 /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter followUsers
 30 20 * * 1 /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py twitter followBack
 
-#instagram, no tira ni uno
+#instagram
 0 */4 * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py instagram publishPhoto
 0 */3 * * *  /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py instagram publishStory
 30 15 * * * /usr/bin/python3 /home/$USER/tool-to-create-digital-identities/main.py instagram replyComments
